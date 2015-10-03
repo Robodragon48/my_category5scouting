c2381d92349bdb7f2c18351dda3e649555d877c0
David Castro <github-email@spamex.com>
2015-10-03 14:00:28 +0000

    Added field to Pit Scouting page

--- cat5scouting/www/js/controllers.js
+++ cat5scouting/www/js/controllers.js
@@ -22,4 +22,15 @@ angular.module('cat5scouting.controllers', [])
 
 .controller('SettingsCtrl', function($scope, $stateParams) {
   
+})
+
+.controller('PitScoutingController', function($scope, $stateParams) {
+  $scope.data = {
+    teamName: null,
+    teamNames: [
+      {id: '1', name: 'Team 1'},
+      {id: '2', name: 'Team 2'},
+      {id: '3', name: 'Team 3'}
+    ]
+  }
 });
