5e14354af1638fb792a36c82b8b56f72db567eda
David Castro <github-email@spamex.com>
2015-10-03 23:32:11 +0000

    Added fields to the Pit Scouting page and corresponding controller

--- cat5scouting/www/js/controllers.js
+++ cat5scouting/www/js/controllers.js
@@ -25,12 +25,89 @@ angular.module('cat5scouting.controllers', [])
 })
 
 .controller('PitScoutingController', function($scope, $stateParams) {
+  ///TODO Convert these to SQLite database calls
   $scope.data = {
     teamName: null,
     teamNames: [
       {id: '1', name: 'Team 1'},
       {id: '2', name: 'Team 2'},
       {id: '3', name: 'Team 3'}
-    ]
+    ],
+    driveMode: null,
+    driveModes: [
+      {id: '0', name: '[Uknown]'},
+      {id: '1', name: 'KOP'},
+      {id: '2', name: 'Mecanum'},
+      {id: '3', name: 'Omni'},
+      {id: '4', name: 'Omni'}
+    ],
+    driveSpeed: null,
+    driveSpeeds: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: 'Slow'},
+      {id: '2', name: 'Medium'},
+      {id: '3', name: 'Fast'}
+    ],
+    driveOverPlatform: null,
+    driveOverPlatformOptions: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: 'Yes'},
+      {id: '2', name: 'No'}
+    ],
+    autonomousCapability: null,
+    autonomousCapabilities: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: 'Bot set'},
+      {id: '2', name: 'Tote set'},
+      {id: '3', name: 'Container set'},
+      {id: '4', name: 'Stacked Tote set'},
+      {id: '5', name: 'None'}
+    ],
+    coopStep: null,
+    coopStepOptions: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: '0'},
+      {id: '2', name: '1'},
+      {id: '3', name: '2'},
+      {id: '4', name: '3'},
+      {id: '5', name: 'None'}
+    ],
+    pickupLoc: null,
+    pickupLocs: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: 'Feed station'},
+      {id: '2', name: 'Landfill'},
+      {id: '3', name: 'Neither'},
+      {id: '4', name: 'Both'}
+    ],
+    maxToteHeight: null,
+    maxToteHeights: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: '0'},
+      {id: '2', name: '1-2'},
+      {id: '3', name: '3-4'},
+      {id: '4', name: '5-6'}
+    ],
+    maxContHeight: null,
+    maxContHeights: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: '0'},
+      {id: '2', name: '1-2'},
+      {id: '3', name: '3-4'},
+      {id: '4', name: '5-6'}
+    ],
+    stackContInd: null,
+    stackContIndOptions: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: 'Yes'},
+      {id: '2', name: 'No'}
+    ],
+    collectContStep: null,
+    collectContStepOptions: [
+      {id: '0', name: '[Unknown]'},
+      {id: '1', name: 'Yes'},
+      {id: '2', name: 'No'}
+    ],
+    note: null
   }
 });
